9eb476fbcf1ce6bf0fbb20fc2635a51c27d75de9
miftahulhussain43 <miftahulhussain43@gmail.com>
2026-05-16 11:31:41 +0000

    fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -613,7 +613,7 @@ export default function Home() {
           <div className="contact-map">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.2!2d77.043!3d28.451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjjCsDI3JzAzLjYiTiA3N8KwMDInMzQuOCJF!5e0!3m2!1sen!2sin!4v1"
-              allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"
+              allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"
               title="Royal Enfield Dealership Location"></iframe>
             <div className="map-overlay-badge">
               <i className="fa-solid fa-location-dot"></i> Royal Enfield Gurugram
